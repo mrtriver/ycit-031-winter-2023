2fc9f72246057956dd73d1c531f47582df447f61
Bobby Connolly <bobbyconnolly@bobbys-mbp.lan>
2023-01-25 21:11:56 -0500

    added register route -- not happy path

--- lecture-8-fullstack/backend/server.js
+++ lecture-8-fullstack/backend/server.js
@@ -16,6 +16,8 @@ const db = new Pool(databaseConnectionSettings)
 
 const app = express()
 
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(`${req.method} request received for ${req.url} from ${req.ip}`)
     next()
@@ -25,6 +27,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8081")
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
     res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+    res.setHeader("Access-Control-Allow-Credentials", "true")
     next()
 })
 
@@ -40,6 +43,12 @@ app.use(
         cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
     })
 )
+
+app.use((req, res, next) => {
+    console.log("userId", req.session.userId)
+
+    next()
+})
 
 app.get("/", async (req, res) => {
     const text = "select name from pets;"
@@ -59,22 +68,50 @@ app.get("/pets", async (req, res) => {
     res.json(result.rows)
 })
 
-app.get("/session", (req, res) => {
-    if (!req.session.views) {
-        req.session.views = 0
+// app.get("/session", (req, res) => {
+//     if (!req.session.views) {
+//         req.session.views = 0
+//     }
+
+//     if (!req.session.country) {
+//         req.session.country = "canada"
+//     }
+
+//     req.session.whatever = "blabla"
+//     req.session.views++
+
+//     res.send(`You viewed this page ${req.session.views} times`)
+// })
+
+app.post("/register", async (req, res) => {
+    console.log("TACO!!!!", req.body)
+
+    try {
+        const selectText = "SELECT id FROM users WHERE username = $1"
+        const selectValues = [req.body.username]
+
+        const selectResult = await db.query(selectText, selectValues)
+
+        console.log("selectResult", selectResult)
+
+        if (selectResult.rowCount > 0) {
+            res.status(400).send("user with that username already exists")
+            return
+        }
+
+        const insertText = "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *" // prettier-ignore
+        const insertValues = [req.body.username, req.body.password]
+        const insertResult = await db.query(insertText, insertValues)
+
+        console.log("result", insertResult)
+
+        req.session.userId = insertResult.rows[0].id
+
+        res.status(200).end()
+    } catch (error) {
+        console.log("ERROR!!", error)
+        res.status(500).send(error.message)
     }
-
-    if (!req.session.country) {
-        req.session.country = "canada"
-    }
-
-    req.session.views++
-
-    res.send(`You viewed this page ${req.session.views} times`)
-})
-
-app.post("/register", (req, res) => {
-    res.status(503).end()
 })
 
 app.post("/login", (req, res) => {
--- lecture-8-fullstack/frontend/src/App.jsx
+++ lecture-8-fullstack/frontend/src/App.jsx
@@ -4,10 +4,18 @@ import { useTable } from "react-table"
 import { useColumns } from "./App.useColumns"
 
 export function App() {
+    const [registerUsername, setRegisterUsername] = useState("")
+    const [registerPassword, setRegisterPassword] = useState("")
+
+    const [loginUsername, setLoginUsername] = useState("")
+    const [loginPassword, setLoginPassword] = useState("")
+
     const [data, setData] = useState([])
 
     useEffect(() => {
-        fetch("http://localhost:3333/pets")
+        fetch("http://127.0.0.1:3333/pets", {
+            credentials: "include",
+        })
             .then((res) => {
                 return res.json()
             })
@@ -21,8 +29,84 @@ export function App() {
     const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
         useTable({ columns, data })
 
+    function handleRegisterSubmit(e) {
+        e.preventDefault()
+        console.log("handle register submit")
+
+        setRegisterUsername("")
+        setRegisterPassword("")
+
+        fetch("http://localhost:3333/register", {
+            method: "POST",
+            body: JSON.stringify({
+                username: registerUsername,
+                password: registerPassword,
+            }),
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then((response) => {
+            console.log("response", response)
+        })
+    }
+
+    function handleLoginSubmit(e) {
+        e.preventDefault()
+        console.log("handle login submit")
+    }
+
     return (
         <div className="App">
+            <div style={{ display: "flex", gap: "8px" }}>
+                <div className="register-form" style={{ maxWidth: "500px" }}>
+                    <h3>Register</h3>
+                    <form onSubmit={handleRegisterSubmit}>
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            value={registerUsername}
+                            onChange={(e) =>
+                                setRegisterUsername(e.target.value)
+                            }
+                        />
+                        <label>Password</label>
+                        <input
+                            type="text"
+                            name="password"
+                            value={registerPassword}
+                            onChange={(e) =>
+                                setRegisterPassword(e.target.value)
+                            }
+                        />
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
+
+                <div className="login-form" style={{ maxWidth: "500px" }}>
+                    <h3>Login</h3>
+
+                    <form onSubmit={handleLoginSubmit}>
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            value={loginUsername}
+                            onChange={(e) => setLoginUsername(e.target.value)}
+                        />
+                        <label>Password</label>
+                        <input
+                            type="text"
+                            name="password"
+                            value={loginPassword}
+                            onChange={(e) => setLoginPassword(e.target.value)}
+                        />
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
+            </div>
+
             <table {...getTableProps()}>
                 <thead>
                     {headerGroups.map((headerGroup) => (
